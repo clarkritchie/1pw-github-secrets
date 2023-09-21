b92b81a1b08afafa001ebc40b1bab55dd8a20737
Clark Ritchie <clark.ritchie@blueboard.com>
2023-09-21 14:00:46 -0700

    cleanup

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ echo "Friendly reminder that some env vars are maybe problematic as they are kno
 echo "See the list hard coded in main.py!!!]"
 echo ""
 
-echo "Usage: ./run.sh docker-swarm dev -- create the dev environment in the docker-swarm project using docker-swarm-dev.env config file"
+echo "Usage: ./run.sh docker-shared dev -- create the dev environment in the docker-shared project using docker-shared-dev.env config file"
 echo "       ./run.sh blueboard staging -- create the destagingv environment in theblueboard repo using blueboard-staging.env config file"
 echo ""
 
@@ -16,8 +16,8 @@ do
         "blueboard")
             export GITHUB_REPO="blueboard"
             break;;
-        "docker")
-            export GITHUB_REPO="docker-swarm"
+        "docker-shared")
+            export GITHUB_REPO="docker-shared"
             break;;
         "other")
             export GITHUB_REPO="foo"
@@ -56,7 +56,6 @@ echo ""
 read -p "Push variables to Github environment ${ENVIRONMENT} to the ${GITHUB_REPO} GitHub repo from the file ${FILE}?  Are you sure? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-
     # echo "Creating a virtual environment"
     python3 -m venv venv
     # echo "Activating the virtual environment"
