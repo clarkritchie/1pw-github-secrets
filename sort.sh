0ecfd504896f86a6cb11bfc95955ac8e9602c057
Clark Ritchie <clark.ritchie@blueboard.com>
2023-09-22 12:07:17 -0700

    adds sort script

---
+++ sort.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+ENV_FILES=( $( ls *.env ) )
+
+for f in "${ENV_FILES[@]}"
+do
+    TMP_FILE=$(mktemp)
+    echo "sorting ${f} to ${TMP_FILE}"
+    sort ${f} --output ${TMP_FILE}
+    # mv ${TMP_FILE} ${f}
+    # rm -f ${TMP_FILE}
+done
